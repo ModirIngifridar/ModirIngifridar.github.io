c7d974ab79aca4a0e06107a8f48f3d0a0a4a4098
UVI-LF1 <marketa.ivankova@lf1.cuni.cz>
2021-02-03 21:01:09 +0100

    header

--- config/site.js
+++ config/site.js
@@ -2,16 +2,16 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'Midnight Mediaeval Thoughts', // Navigation and Site Title
   titleAlt: 'Midnight Mediaeval Thoughts', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  description: 'A blog on Old Norse literature and similar topics, written around midnight, hence its name Midnight Mediaeval Thoughts',
+  url: 'https://modiringifridar.github.io', // Domain of your site. No trailing slash!
+  siteUrl: 'https://modiringifridar.github.io', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'MidMediaevalT', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Modir Ingifridar', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
   twitter: '@justinformentin', // Twitter Username
